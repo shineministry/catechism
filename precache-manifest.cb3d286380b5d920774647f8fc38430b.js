e1de8bead8a674e3c3350d85ccdd482e0241a4d9
Shine Ministry <shinumaths989@gmail.com>
2025-05-14 23:44:36 +0530

    Updates

---
+++ precache-manifest.cb3d286380b5d920774647f8fc38430b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05dab67e83e30869a6d2f589db37a1b5",
+    "url": "/catechism/index.html"
+  },
+  {
+    "revision": "d9a2aac5dc7cb2d34368",
+    "url": "/catechism/static/css/main.df1fe396.chunk.css"
+  },
+  {
+    "revision": "36fa32a09b5af9134a71",
+    "url": "/catechism/static/js/2.ccfe72b6.chunk.js"
+  },
+  {
+    "revision": "d9a2aac5dc7cb2d34368",
+    "url": "/catechism/static/js/main.239c9fb1.chunk.js"
+  },
+  {
+    "revision": "89ee3beb61e71ed07140",
+    "url": "/catechism/static/js/runtime~main.3c531b9b.js"
+  },
+  {
+    "revision": "07883e93734b98cae0f7b9c55d287250",
+    "url": "/catechism/static/media/github.07883e93.svg"
+  },
+  {
+    "revision": "7e877805dd1d832df8a4bd6ba9b890c0",
+    "url": "/catechism/static/media/psl.7e877805.cjs"
+  }
+]);
